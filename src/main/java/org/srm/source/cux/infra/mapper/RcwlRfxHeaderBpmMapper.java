0d82d7dabc3e718a25be90c37c77997894b4e124
yongbo.ji <yongbo.ji@hand-china.com>
2021-05-19 14:49:12 +0800

    询报价发布审批 2021/05/17 1.7版本:元素

--- src/main/java/org/srm/source/cux/infra/mapper/RcwlRfxHeaderBpmMapper.java
+++ src/main/java/org/srm/source/cux/infra/mapper/RcwlRfxHeaderBpmMapper.java
@@ -3,10 +3,14 @@ package org.srm.source.cux.infra.mapper;
 import org.apache.ibatis.annotations.Param;
 import org.srm.source.cux.domain.vo.*;
 import org.srm.source.rfx.domain.entity.RfxHeader;
+import org.srm.source.share.api.dto.EvaluateIndicDTO;
+import org.srm.source.share.domain.entity.EvaluateIndic;
 
 import java.util.List;
 
 public interface RcwlRfxHeaderBpmMapper {
+
+    List<EvaluateIndic> rcwlQueryEvaluateIndicate(EvaluateIndicDTO evaluateIndicDTO);
 
     RcwlSendBpmData prepareDate(@Param("quotationHeaderId")Long organizationId,@Param("rfxHeader") RfxHeader rfxHeader);
 
