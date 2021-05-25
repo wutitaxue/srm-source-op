66faad0dd7b8be478fda90deca93c69dd02ed81c
yongbo.ji <yongbo.ji@hand-china.com>
2021-05-25 14:46:08 +0800

    询报价发布审批 2021/05/24 2.1版本:封装返回的url为json

---
+++ src/main/java/org/srm/source/cux/rfx/api/controller/dto/UrlDTO.java
@@ -0,0 +1,13 @@
+package org.srm.source.cux.rfx.api.controller.dto;
+
+public class UrlDTO {
+    private String backUrl;
+
+    public String getBackUrl() {
+        return backUrl;
+    }
+
+    public void setBackUrl(String backUrl) {
+        this.backUrl = backUrl;
+    }
+}
