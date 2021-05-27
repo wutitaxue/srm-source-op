465e48056a10c54f01956b57bf7bb3b805cd5da4
yongbo.ji <yongbo.ji@hand-china.com>
2021-05-27 21:20:38 +0800

    feature-opd-1327 复制历史单据两个备用字段给值0 1.3版本:

--- src/main/java/org/srm/source/cux/rfx/app/service/v2/impl/RcwlRfxHeaderServiceV2Impl.java
+++ src/main/java/org/srm/source/cux/rfx/app/service/v2/impl/RcwlRfxHeaderServiceV2Impl.java
@@ -1,4 +1,4 @@
-package org.srm.source.cux.rfx.app.service.impl;
+package org.srm.source.cux.rfx.app.service.v2.impl;
 
 import org.hzero.core.base.BaseConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -82,62 +82,4 @@ public class RcwlRfxHeaderServiceV2Impl extends RfxHeaderServiceV2Impl {
 
     }
 
-
-
-    @Override
-    public RfxHeader copyRfxHeader(Long rfxHeaderId) {
-        RfxHeader rfxHeader = (RfxHeader)this.rfxHeaderRepository.selectByPrimaryKey(rfxHeaderId);
-        Assert.notNull(rfxHeader, "error.rfx_header.not_found");
-        rfxHeader.setCopyRfxHeaderId(rfxHeaderId);
-        rfxHeader.setRfxHeaderId((Long)null);
-        rfxHeader.setObjectVersionNumber((Long)null);
-        rfxHeader.setRfxStatus("NEW");
-        rfxHeader.setQuotationStartDate((Date)null);
-        rfxHeader.setQuotationEndDate((Date)null);
-        rfxHeader.setRoundNumber(1L);
-        rfxHeader.setVersionNumber(1L);
-        rfxHeader.setReleasedDate((Date)null);
-        rfxHeader.setReleasedBy((Long)null);
-        rfxHeader.setTerminatedDate((Date)null);
-        rfxHeader.setTerminatedBy((Long)null);
-        rfxHeader.setTerminatedRemark((String)null);
-        rfxHeader.setApprovedDate((Date)null);
-        rfxHeader.setApprovedBy((Long)null);
-        rfxHeader.setApprovedRemark((String)null);
-        rfxHeader.setTimeAdjustedDate((Date)null);
-        rfxHeader.setTimeAdjustedBy((Long)null);
-        rfxHeader.setTimeAdjustedRemark((String)null);
-        rfxHeader.setPretrailRemark((String)null);
-        rfxHeader.setPretrialUserId((Long)null);
-        rfxHeader.setPretrialUuid((String)null);
-        rfxHeader.setPreAttachmentUuid((String)null);
-        rfxHeader.setBackPretrialRemark((String)null);
-        rfxHeader.setPretrialStatus((String)null);
-        rfxHeader.setHandDownDate((Date)null);
-        rfxHeader.setStartQuotationRunningDuration((BigDecimal)null);
-        rfxHeader.setLatestQuotationEndDate((Date)null);
-        rfxHeader.setBargainStatus((String)null);
-        rfxHeader.setBargainEndDate((Date)null);
-        rfxHeader.setCheckRemark((String)null);
-        rfxHeader.setCheckAttachmentUuid((String)null);
-        rfxHeader.setCheckedBy((Long)null);
-        rfxHeader.setCheckFinishedDate((Date)null);
-        rfxHeader.setTotalCost((BigDecimal)null);
-        rfxHeader.setCostRemark((String)null);
-        rfxHeader.setCurrentSequenceNum(BidConstants.BidHeader.CurrentSequenceNum.FIRST);
-        rfxHeader.setSourceFrom("MANUAL");
-        rfxHeader.setScoreProcessFlag(BaseConstants.Flag.NO);
-        rfxHeader.setProjectLineSectionId((Long)null);
-        rfxHeader.setSourceProjectId((Long)null);
-        rfxHeader.setMultiSectionFlag(BaseConstants.Flag.NO);
-        //招采时复制历史单据时需要自动给表： ssrc_rfx_header
-        //的字段：attribute_varchar2、attribute_varchar3、attribute_varchar4、attribute_varchar5、attribute_varchar6、attribute_varchar7自动赋值为0
-        rfxHeader.setAttributeVarchar2("0");
-        rfxHeader.setAttributeVarchar3("0");
-        rfxHeader.setAttributeVarchar4("0");
-        rfxHeader.setAttributeVarchar5("0");
-        rfxHeader.setAttributeVarchar6("0");
-        rfxHeader.setAttributeVarchar7("0");
-        return ((RfxHeaderService)this.self()).saveOrUpdateHeader(rfxHeader);
-    }
 }
