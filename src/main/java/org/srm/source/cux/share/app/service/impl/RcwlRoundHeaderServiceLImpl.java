124b98b9ba721eb4841ab8d9357c992f72fffcec
24769 <kaibo.li@hand-china.com>
2021-06-07 10:35:29 +0800

    kaibo.li 多轮报价重算商务分,无专家评分汇总不重算 3.8

--- src/main/java/org/srm/source/cux/share/app/service/impl/RcwlRoundHeaderServiceLImpl.java
+++ src/main/java/org/srm/source/cux/share/app/service/impl/RcwlRoundHeaderServiceLImpl.java
@@ -1,5 +1,6 @@
 package org.srm.source.cux.share.app.service.impl;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -87,17 +88,19 @@ public class RcwlRoundHeaderServiceLImpl extends RoundHeaderServiceImpl {
         if (!Long.valueOf(1).equals(roundHeaderDb.getQuotationRoundNumber())) {
             // 获取专家评分汇总信息
             List<EvaluateSummary> evaluateSummarys =  this.rcwlRfxQuotationLineRepository.queryEvaluateSummary(new EvaluateSummary(rfxHeader.getTenantId(),rfxHeader.getRfxHeaderId(),"RFX",rfxHeader.getRoundNumber()));
-            for (EvaluateSummary evaluateSummary: evaluateSummarys) {
-                // 无效投标
-                evaluateSummary.setBusinessScore(BigDecimal.ZERO);
-                evaluateSummary.setScore(BigDecimal.ZERO);
-            }
-            LOGGER.info("BusinessScore : {} ", evaluateSummarys.get(0).getBusinessScore());
-            // 重算商务分之前，清空原商务分，总分，排名
-            this.evaluateSummaryRepository.batchUpdateOptional(evaluateSummarys,"businessScore","score","scoreRank");
+            if (CollectionUtils.isNotEmpty(evaluateSummarys)) {
+                for (EvaluateSummary evaluateSummary: evaluateSummarys) {
+                    // 无效投标
+                    evaluateSummary.setBusinessScore(BigDecimal.ZERO);
+                    evaluateSummary.setScore(BigDecimal.ZERO);
+                }
+                LOGGER.info("BusinessScore : {} ", evaluateSummarys.get(0).getBusinessScore());
+                // 重算商务分之前，清空原商务分，总分，排名
+                this.evaluateSummaryRepository.batchUpdateOptional(evaluateSummarys,"businessScore","score","scoreRank");
 
-            List<Long> invalidQuotationHeaderIdList = this.rcwlEvaluateScoreLineService.getInvalidQuotationHeaderIdList(rfxHeader);
-            this.rcwlEvaluateScoreLineService.multipleRoundAutoEvaluateScore(new AutoScoreDTO(tenantId, "RFX", sourceHeaderId, invalidQuotationHeaderIdList));
+                List<Long> invalidQuotationHeaderIdList = this.rcwlEvaluateScoreLineService.getInvalidQuotationHeaderIdList(rfxHeader);
+                this.rcwlEvaluateScoreLineService.multipleRoundAutoEvaluateScore(new AutoScoreDTO(tenantId, "RFX", sourceHeaderId, invalidQuotationHeaderIdList));
+            }
         }
     }
 }
