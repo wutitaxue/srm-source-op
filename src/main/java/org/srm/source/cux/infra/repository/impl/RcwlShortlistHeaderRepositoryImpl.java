4a86e3cf748d7208433af9e8c93b2290a7d7fb60
30694 <xiaopeng.li02@hand-china.com>
2021-10-21 15:27:35 +0800

    供应商手机号错误调整

--- src/main/java/org/srm/source/cux/infra/repository/impl/RcwlShortlistHeaderRepositoryImpl.java
+++ src/main/java/org/srm/source/cux/infra/repository/impl/RcwlShortlistHeaderRepositoryImpl.java
@@ -339,7 +339,7 @@ public class RcwlShortlistHeaderRepositoryImpl extends BaseRepositoryImpl<RcwlSh
         SupplierVO supplierVO = rcwlShortlistHeaderMapper.currentSupplierInfo(userDetails.getUserId());
         //获取联系人及电话
         if (ObjectUtils.allNotNull(supplierVO)) {
-            Long companyId = supplierVO.getCompanyId();
+            Long companyId = supplierVO.getSupplierId();
             List<CompanyContactVO> companyContactVOS = rcwlShortlistHeaderMapper.selectContactsByCompanyId(companyId);
             if (CollectionUtils.isNotEmpty(companyContactVOS)) {
                 supplierVO.setContact(companyContactVOS.get(0).getContact());
