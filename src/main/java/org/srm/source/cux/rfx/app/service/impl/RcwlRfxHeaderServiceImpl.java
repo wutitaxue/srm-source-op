690563e1c97fadc974f64bda5f19f9996fcbb907
30694 <xiaopeng.li02@hand-china.com>
2021-11-19 13:59:38 +0800

    【询报价】寻源模板为直委时，增加供应商生命周期阶段“注册”

--- src/main/java/org/srm/source/cux/rfx/app/service/impl/RcwlRfxHeaderServiceImpl.java
+++ src/main/java/org/srm/source/cux/rfx/app/service/impl/RcwlRfxHeaderServiceImpl.java
@@ -2,31 +2,44 @@ package org.srm.source.cux.rfx.app.service.impl;
 
 import io.choerodon.core.exception.CommonException;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hzero.core.base.BaseConstants;
 import org.hzero.mybatis.domian.Condition;
 import org.hzero.mybatis.util.Sqls;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.srm.source.bid.api.dto.BiddingWorkDTO;
 import org.srm.source.bid.infra.constant.BidConstants;
 import org.srm.source.cux.rfx.infra.constant.RcwlSourceConstant;
+import org.srm.source.rfx.api.dto.HeaderQueryDTO;
+import org.srm.source.rfx.api.dto.RfxHeaderDTO;
 import org.srm.source.rfx.app.service.RfxHeaderService;
+import org.srm.source.rfx.app.service.RfxMemberService;
 import org.srm.source.rfx.app.service.impl.RfxHeaderServiceImpl;
 import org.srm.source.rfx.domain.entity.RfxHeader;
+import org.srm.source.rfx.domain.entity.RfxLineItem;
+import org.srm.source.rfx.domain.entity.RfxMember;
+import org.srm.source.rfx.domain.repository.CommonQueryRepository;
 import org.srm.source.rfx.domain.repository.RfxHeaderRepository;
+import org.srm.source.rfx.domain.repository.RfxLineItemRepository;
+import org.srm.source.rfx.domain.repository.RfxMemberRepository;
 import org.srm.source.rfx.domain.vo.RfxFullHeader;
 import org.srm.source.share.app.service.EvaluateIndicAssignService;
-import org.srm.source.share.domain.entity.EvaluateExpert;
-import org.srm.source.share.domain.entity.EvaluateIndic;
-import org.srm.source.share.domain.entity.SourceTemplate;
+import org.srm.source.share.app.service.SourceTemplateService;
+import org.srm.source.share.domain.entity.*;
 import org.srm.source.share.domain.repository.EvaluateExpertRepository;
+import org.srm.source.share.domain.repository.ProjectLineSectionRepository;
+import org.srm.source.share.domain.repository.RoundHeaderDateRepository;
 import org.srm.web.annotation.Tenant;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Tenant(RcwlSourceConstant.TENANT_CODE)
@@ -37,6 +50,21 @@ public class RcwlRfxHeaderServiceImpl extends RfxHeaderServiceImpl {
     private EvaluateExpertRepository evaluateExpertRepository;
     @Autowired
     private EvaluateIndicAssignService evaluateIndicAssignService;
+    @Autowired
+    private SourceTemplateService sourceTemplateService;
+    @Autowired
+    private RfxMemberRepository rfxMemberRepository;
+    @Autowired
+    private CommonQueryRepository commonQueryRepository;
+    @Autowired
+    @Lazy
+    private RfxMemberService rfxMemberService;
+    @Autowired
+    private RoundHeaderDateRepository roundHeaderDateRepository;
+    @Autowired
+    private ProjectLineSectionRepository projectLineSectionRepository;
+    @Autowired
+    private RfxLineItemRepository rfxLineItemRepository;
     @Override
     public RfxHeader copyRfxHeader(Long rfxHeaderId) {
         RfxHeader rfxHeader = (RfxHeader)this.rfxHeaderRepository.selectByPrimaryKey(rfxHeaderId);
@@ -166,4 +194,59 @@ public class RcwlRfxHeaderServiceImpl extends RfxHeaderServiceImpl {
             }
         }
     }
+
+    public RfxHeaderDTO selectOneRfxHeader(HeaderQueryDTO headerQueryDTO) {
+        RfxHeaderDTO rfxHeaderDTO = this.rfxHeaderRepository.selectOneRfxHeader(headerQueryDTO);
+        SourceTemplate sourceTemplate = this.sourceTemplateService.selectByPrimaryKey(rfxHeaderDTO.getTemplateId());
+        rfxHeaderDTO.setFastBidding(sourceTemplate.getFastBidding());
+        int count = this.rfxMemberRepository.selectCount(new RfxMember(rfxHeaderDTO.getTenantId(), rfxHeaderDTO.getRfxHeaderId(), "OPENED_BY", BaseConstants.Flag.YES));
+        if (BaseConstants.Flag.YES.equals(sourceTemplate.getOpenerFlag()) && BaseConstants.Flag.YES.equals(rfxHeaderDTO.getSealedQuotationFlag()) && count == 0) {
+            rfxHeaderDTO.setQuotationEndDateChangeFlag(BaseConstants.Flag.YES);
+        } else {
+            rfxHeaderDTO.setQuotationEndDateChangeFlag(BaseConstants.Flag.NO);
+        }
+
+        String allowSourceSupplierStages = (String)this.commonQueryRepository.getAllowSourceSupplierStages(rfxHeaderDTO.getTenantId()).stream().filter(StringUtils::isNotEmpty).collect(Collectors.joining(","));
+        //当寻源模板为直委，去掉对注册的筛选
+        if("直委".equals(rfxHeaderDTO.getTemplateName())) allowSourceSupplierStages+=",注册";
+        rfxHeaderDTO.setAllowSourceSupplierStages(allowSourceSupplierStages);
+        this.rfxMemberService.handleRfxHeaderPermission(Collections.singletonList(rfxHeaderDTO));
+        if (rfxHeaderDTO.getCompanyId() == -1L) {
+            rfxHeaderDTO.setCompanyId((Long)null);
+        }
+
+        if (StringUtils.isNotBlank(sourceTemplate.getRoundQuotationRule()) && sourceTemplate.getRoundQuotationRule().contains("AUTO")) {
+            List<RoundHeaderDate> roundHeaderDates = this.roundHeaderDateRepository.selectByCondition(Condition.builder(RoundHeaderDate.class).where(Sqls.custom().andEqualTo("sourceHeaderId", rfxHeaderDTO.getRfxHeaderId()).andEqualTo("tenantId", rfxHeaderDTO.getTenantId()).andEqualTo("autoFlag", BaseConstants.Flag.YES).andEqualTo("roundNumber", rfxHeaderDTO.getRoundNumber())).build());
+            rfxHeaderDTO.setRoundHeaderDates(roundHeaderDates);
+            if (!"NEW".equals(rfxHeaderDTO.getRfxStatus()) && !"ROUNDED".equals(rfxHeaderDTO.getRfxStatus()) && !"RELEASE_APPROVING".equals(rfxHeaderDTO.getRfxStatus()) && !"RELEASE_REJECTED".equals(rfxHeaderDTO.getRfxStatus())) {
+                RoundHeaderDate roundHeaderDateParam = new RoundHeaderDate(rfxHeaderDTO.getRfxHeaderId(), rfxHeaderDTO.getTenantId(), rfxHeaderDTO.getRoundNumber());
+                roundHeaderDateParam.setRoundQuotationEndDate(new Date());
+                roundHeaderDateParam.setAutoFlag(BaseConstants.Flag.YES);
+                int finishedCount = this.roundHeaderDateRepository.selectFinishedRoundCount(roundHeaderDateParam);
+                if ((long)finishedCount < rfxHeaderDTO.getQuotationRounds()) {
+                    rfxHeaderDTO.setCurrentQuotationRound((long)finishedCount + 1L);
+                }
+            }
+        }
+
+        if (BaseConstants.Flag.YES.equals(rfxHeaderDTO.getMultiSectionFlag())) {
+            ProjectLineSection projectLineSectionParam = new ProjectLineSection();
+            projectLineSectionParam.setTenantId(rfxHeaderDTO.getTenantId());
+            projectLineSectionParam.setTempSourceHeaderId(rfxHeaderDTO.getRfxHeaderId());
+            projectLineSectionParam.setSourceProjectId(rfxHeaderDTO.getSourceProjectId());
+            List<ProjectLineSection> projectLineSections = this.projectLineSectionRepository.listProjectLineSections(projectLineSectionParam);
+            Iterator var13 = projectLineSections.iterator();
+
+            while(var13.hasNext()) {
+                ProjectLineSection projectLineSection = (ProjectLineSection)var13.next();
+                int itemCount = this.rfxLineItemRepository.selectCountByCondition(Condition.builder(RfxLineItem.class).where(Sqls.custom().andEqualTo("rfxHeaderId", rfxHeaderDTO.getRfxHeaderId()).andEqualTo("projectLineSectionId", projectLineSection.getProjectLineSectionId())).build());
+                projectLineSection.setProjectItemCount((long)itemCount);
+            }
+
+            rfxHeaderDTO.setProjectLineSections(projectLineSections);
+        }
+
+        return rfxHeaderDTO;
+    }
+
 }
